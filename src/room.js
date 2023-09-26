ee8895659e9487a05d57d9f8e7e5c08b3e383d3f
keijun-kumagai <keijun.kumagai@dena.com>
2023-09-27 03:20:40 +0900

    feat(all): shiritori rules

--- src/room.js
+++ src/room.js
@@ -4,42 +4,78 @@ class Rule {
     this.func = func;
   }
 
-  validate(value) {
-    return this.func(value);
+  validate(value, previousValue = "") {
+    return this.func(value, previousValue);
   }
 }
 
-const x = ({ index, expect }) => {
-  return (actual) => {
-    return actual[index] === expect;
-  };
-};
-
-const startWithKi = new Rule({
-  note: "ワードの最初の文字は『き』",
-  func: x({ index: 0, expect: "き" }),
+const shiritori = new Rule({
+  note: "しりとり",
+  func: (v, pv) => (pv === "" ? false : v[0] === pv[pv.length - 1]),
 });
+
+const shiritori1 = new Rule({
+  note: "しりとり",
+  func: (v, _) => v !== "",
+});
+
+const XinY = ({ x, y }) => {
+  return new Rule({
+    note: `ワードの${y + 1}番目の文字が『${x}』`,
+    func: (v, _) => v[y] === x,
+  });
+};
 
 export class Room {
   constructor({ name, color }) {
     this._name = name;
     this._color = color;
     this._words = [
-      { value: "", status: "？", rules: [startWithKi] },
-      { value: "", status: "？", rules: [startWithKi] },
-      { value: "", status: "？", rules: [startWithKi] },
+      {
+        value: "",
+        status: "？",
+        note: "",
+        rules: [shiritori1, XinY({ x: "き", y: 0 })],
+      },
+      {
+        value: "",
+        status: "？",
+        note: "",
+        rules: [shiritori, XinY({ x: "ひ", y: 0 })],
+      },
+      {
+        value: "",
+        status: "？",
+        note: "",
+        rules: [shiritori, XinY({ x: "に", y: 0 })],
+      },
     ];
   }
 
   update({ index, value }) {
     this._words[index].value = value;
-    let x = "？";
-    if (value !== "") {
-      x = this._words[index].rules.map((y) => y.func(value)).every((z) => z)
-        ? "OK"
-        : "NG";
+
+    for (let i = 0; i < this._words.length; i++) {
+      if (this._words[i].value === "") {
+        this._words[i].status = "？";
+        continue;
+      }
+
+      let status = "OK";
+      let notes = [];
+      const rules = this._words[i].rules;
+      const previousValue = i === 0 ? "" : this._words[i - 1].value;
+      for (let j = 0; j < rules.length; j++) {
+        const bool = rules[j].validate(this._words[i].value, previousValue);
+        notes.push((bool ? "◯ " : "✕ ") + rules[j].note);
+        if (!bool) {
+          status = "NG";
+        }
+      }
+
+      this._words[i].status = status;
+      this._words[i].note = notes.join(" ");
     }
-    this._words[index].status = x;
   }
 
   words() {
@@ -47,11 +83,7 @@ export class Room {
       index: i,
       value: x.value,
       status: x.status,
-      note: x.rules
-        .map(
-          (y) => (x.value === "" ? "" : y.func(x.value) ? "◯ " : "✕ ") + y.note
-        )
-        .join(","),
+      note: x.value === "" ? x.rules.map((y) => y.note).join(" ") : x.note,
     }));
   }
 
